7f86d0ebc57c449adfb3ec78eeec48e0c647b2d0
L A <laurenceunabia@gmail.com>
2024-12-23 23:40:41 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     }, [])
 
     return (
-        <div className="max-w-screen-2xl mx-auto">
+        <div className="max-w-screen-2xl mx-auto overflow-x-hidden">
             <Hero/>
             <About/>
             <MissionVision/>
